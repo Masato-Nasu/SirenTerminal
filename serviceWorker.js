4f7749b595d2e444da07f828bd7c074106787dd8
Masato-Nasu <1bitexist@gmail.com>
2025-10-26 10:34:34 +0900

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='siren-terminal-v3';
+const CACHE_NAME='siren-terminal-v1';
 const FILES_TO_CACHE=['./index.html','./style.css','./script.js','./manifest.json','./gpt4all/gpt4all.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE)));});
 self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));});
